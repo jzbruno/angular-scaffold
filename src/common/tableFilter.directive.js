d15499f5ff0343e3d3af7cdb3db50b31bba0f4a5
Jack Bruno <jzbruno@gmail.com>
2015-01-24 20:19:08 -0700

    Add table filtering.

---
+++ src/common/tableFilter.directive.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('tableFilter', tableFilter);
+
+    function tableFilter() {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'common/tableFilter.html',
+            scope: {
+                fields: '=',
+                filter: '=',
+                keywords: '='
+            },
+            link: link
+        };
+
+        function link(scope) {
+            scope.addFilter = addFilter;
+            scope.removeFilter = removeFilter;
+
+            function addFilter() {
+                scope.filter || (scope.filter = {});
+                scope.filter[scope.filterName] = scope.filterValue;
+            }
+
+            function removeFilter(field) {
+                delete scope.filter[field];
+
+                if (Object.keys(scope.filter).length === 0) {
+                    scope.filter = null;
+                }
+            }
+        }
+    }
+})();
